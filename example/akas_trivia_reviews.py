569a9b7f50a647c2f76dee694e55590ca5867a07
⌯ Ꭺɴᴏɴʏᴍᴏᴜꜱ | ×͜× | <155171427+NBBotz@users.noreply.github.com>
2026-01-24 11:26:27 +0530

    Create akas_trivia_reviews.py

---
+++ example/akas_trivia_reviews.py
@@ -0,0 +1,31 @@
+from imdbkit import IMDBKit
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+
+kit = IMDBKit()
+
+movies_list = [
+    "tt0133093",   # The Matrix (movie)
+    "tt1520211",  # The Walking Dead (tvSeries)
+    "tt30406366",  # The Walking Dead: Daryl Dixon (tvMiniSeries)
+    "tt1589921",  # The Walking Dead S01E01 (tvEpisode series)
+    "tt12326830", #  'The Sandman' (podcastSeries)
+    "tt15110916", # 'The Sandman' (2022)  s01e01 (podcastEpisode)
+    "tt2080323", # Hotel Desire (short)
+    "tt36048590", # Marc Maron: Panicked (tvSpecial)
+    "tt6582384" , # Red Nose Day Actually (tvShort)
+    "tt33238076", # Mafia: The Old Country (videoGame)
+    "tt11771594", # American Pie Presents: Girls' Rules (video)
+    "tt37195825" , # Talking Heads: Psycho Killer (musicVideo)
+    "tt33501878", #title akas
+]
+
+for imdb_id in movies_list:
+    movie_akas = kit.get_akas(imdb_id)
+    movie_reviews = kit.get_reviews(imdb_id)
+    movie_trivia = kit.get_trivia(imdb_id)
+    print("##########################################################################")
+    print(movie_akas['akas'])
+    print(f"Reviews: {movie_reviews[:5]}")
+    print(f"Trivia: {movie_trivia[:5]}")
